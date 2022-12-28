41da47bb864f577c103789e83156ffa815e2d62a
Your Name <shuvongkarroy13@gmail.com>
2022-12-28 12:10:26 +0600

    Identifiers commit

---
+++ src/IdentifiersTest.java
@@ -0,0 +1,19 @@
+public class IdentifiersTest {
+
+    public static void main(String[] args) {
+
+        int age = 30;
+        //Here age is an identifier.
+
+        double score = 30.2;
+        //Here score is an identifier.
+
+        long id = 1022532115;
+        //Here id is an identifier.
+
+        String string = "England";
+        //Here string is an identifier.
+
+        System.out.println(string);
+    }
+}
